cf39e4611eb6f568095962425846073beb77f9ed
gabmft <gabitumaftei@yahoo.com>
2024-03-11 12:19:36 +0200

    24

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Weather from './components/weather';
+
+function App() {
+    return (
+    <div>
+      <div className="header">
+        <h1 className="header-title">Weather Dashboard</h1>
+      </div>
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
